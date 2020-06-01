87c7f2077235cf7954c316c1512d7deac13ff9af
Elikworld <koladeodutoye9913@gmail.com>
2020-06-01 21:29:02 +0100

    feat: added the passenger sign-in plus token check

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,62 +1,111 @@
-const statusCode = require("http-status");
+const statusCode = require('http-status');
 const bcrypt = require('bcryptjs');
-const sendMail = require("../utils/sendMail");
-const { Passenger } = require("../models");
+const jwt = require('jsonwebtoken');
+const sendMail = require('../utils/sendMail');
+const { Passenger } = require('../models');
+const config = require('../config/index');
 
 exports.userSignUp = async (req, res) => {
-    const { email, password, mobile_num, fullname, address } = req.body;
-    const userExists = await Passenger.findOne({ email });
-    if (userExists) {
-        return res.status(statusCode.CONFLICT).json({
-            message: "User already exists"
-        });
-    }
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new Passenger({
-            email, 
-            fullname,
-            password: hashedPassword,
-            address,
-            mobile_num           
-        });
-        const savedUser = await user.save();
-        sendMail.sendConfirmEmail(savedUser); // sends activation link
-        return res.status(statusCode.OK).json({
-            message: "Account created. Please check your email, you will receive a confirmation mail in few minutes."
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json({
-            message: "Something went wrong. Try again."
-        });
-    }
+	const { email, password, mobile_num, fullname, address } = req.body;
+	const userExists = await Passenger.findOne({ email });
+	if (userExists) {
+		return res.status(statusCode.CONFLICT).json({
+			message: 'User already exists',
+		});
+	}
+	try {
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const user = new Passenger({
+			email,
+			fullname,
+			password: hashedPassword,
+			address,
+			mobile_num,
+		});
+		const savedUser = await user.save();
+		sendMail.sendConfirmEmail(savedUser); // sends activation link
+		return res.status(statusCode.OK).json({
+			message:
+				'Account created. Please check your email, you will receive a confirmation mail in few minutes.',
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(statusCode.INTERNAL_SERVER_ERROR).json({
+			message: 'Something went wrong. Try again.',
+		});
+	}
 };
 
 exports.activateUserAccount = async (req, res) => {
-    const { email } = req.params;
-    try {
-        const userFound = await Passenger.findOneAndUpdate({ email }, { is_verified: true });
-        if (!userFound) {
-            return res.status(statusCode.NOT_FOUND).json({
-                message: `Account ${email} doesn't exist`
-            });
-        }   
-        return res.status(statusCode.OK).json({
-            message: "Account verification successful. You can login"
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(statusCode.SERVICE_UNAVAILABLE).json({
-            message: 'Something went wrong. Please try again..'
-        });
-    }
+	const { email } = req.params;
+	try {
+		const userFound = await Passenger.findOneAndUpdate(
+			{ email },
+			{ is_verified: true }
+		);
+		if (!userFound) {
+			return res.status(statusCode.NOT_FOUND).json({
+				message: `Account ${email} doesn't exist`,
+			});
+		}
+		return res.status(statusCode.OK).json({
+			message: 'Account verification successful. You can login',
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(statusCode.SERVICE_UNAVAILABLE).json({
+			message: 'Something went wrong. Please try again..',
+		});
+	}
 };
 
-exports.userSignIn = (req, res) => {
+exports.userSignIn = async (req, res) => {
+	const { email, password } = req.body;
 
+	const validUser = await Passenger.findOne({ email });
+
+	if (!validUser) {
+		return res.status(statusCode.NOT_FOUND).json({
+			message: `Account ${email} doesn't exist`,
+		});
+	}
+
+	try {
+		if (!validUser.is_verified) {
+			return res.status(statusCode.UNAUTHORIZED).json({
+				message: 'Please, verify your email first',
+			});
+		}
+
+		const confirmedPassword = await bcrypt.compare(
+			password,
+			validUser.password
+		);
+		if (!confirmedPassword) {
+			return res.status(statusCode.FORBIDDEN).json({
+				message: 'Invalid password',
+			});
+		}
+
+		const accessToken = await jwt.sign(
+			{ userId: validUser._id, email: validUser.email },
+			config.SECRET_KEY,
+			{ expiresIn: '1d' }
+		);
+
+		return res.status(statusCode.OK).json({
+			message: 'Passenger signed in successfully',
+			passsenger: validUser,
+			accessToken: accessToken,
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(statusCode.SERVICE_UNAVAILABLE).json({
+			message: 'Something went wrong. Please try again..',
+		});
+	}
 };
 
 exports.userLogOut = (req, res) => {
-
-}
+	//res.json({ message: 'done' });
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,19 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { userSignUp, userSignIn, activateUserAccount } = require("../controllers/auth.controllers");
-const { validate, userValidationRules } = require("../middlewares/userValidation");
+const {
+	userSignUp,
+	userSignIn,
+	activateUserAccount,
+	userLogOut,
+} = require('../controllers/auth.controllers');
+const {
+	validate,
+	userValidationRules,
+	userSignInValidationRules,
+} = require('../middlewares/userValidation');
 
-router.post("/register", userValidationRules(), validate, userSignUp); // register user
-router.patch("/verify/:email", activateUserAccount); //verify account before user can login
-
+router.post('/register', userValidationRules(), validate, userSignUp); // register user
+router.patch('/verify/:email', activateUserAccount); //verify account before user can login
+router.post('/login', userSignInValidationRules(), validate, userSignIn);
+router.post('/signout', userSignInValidationRules(), userLogOut);
 module.exports = router;
